e9c84ad6b356e73758025a6cd7c5d77ac292fe81
Tanner Schilperoort <tannerschilperoort@gmail.com>
2024-06-13 20:32:37 -0400

    Create main.py, set constants

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+FPS = 60
+
+WIDTH, HEIGHT = 800, 800
+ROWS = 4
+COLS = 4
+
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
+
+OUTLINE_COLOR = (187, 173, 160)
+OUTLINE_THICKNESS = 10
+BACKGROUND_COLOR = (205, 192, 180)
+FONT_COLOR = (119, 110, 101)
+
+FONT = pygame.font.SysFont("comicsans", 60, bold=True)
+MOVE_VEL = 20
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("2048")
